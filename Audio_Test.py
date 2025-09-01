e2cdbd9cfe5029823d52afe41c67638b5da4d38c
rafaarklu <169057187+rafaarklu@users.noreply.github.com>
2025-09-01 08:52:41 -0300

    SCRUM-19-Configurar-ambiente-do-backend-(FastAPI)

--- Audio_Test.py
+++ Audio_Test.py
@@ -2,18 +2,21 @@ from openai import OpenAI
 from config import api_key
 
 client = OpenAI(api_key=api_key)
-audio_file = open(".\Audio\q_isso_renan.mp3", "rb")
 
-transcription = client.audio.transcriptions.create(
-    file=audio_file,
-    model="whisper-1", 
-    response_format="verbose_json",
-    timestamp_granularities=["word"]
-)
-
-print(transcription.words)
-
-
-
-#for word in trascription.words:
-#   print(f"{(word.start * 1000):.0f}\t{(word.end * 1000):.0f}\t{word.word}")
+def print_word_timings():
+    audio_path = "./Audio/q_isso_renan.mp3"
+    with open(audio_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-1", 
+            response_format="verbose_json",
+            timestamp_granularities=["word"]
+        )
+    return [
+        {
+            "start_ms": int(word.start * 1000),
+            "end_ms": int(word.end * 1000),
+            "word": word.word
+        }
+        for word in transcription.words
+    ]
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from Audio_Test import print_word_timings
+from fastapi.responses import JSONResponse
 
 
+# LEMBRETE!!!!!!!! == SEMPRE QUE ABRIR A API NO NAEGADOR ADICIONAR /docs NO FINAL DO LINK 
+# PARA VER A DOCUMENTAÇÃO AUTOMÁTICA
+# EXEMPLO: http://http://127.0.0.1:8000/docs
 app = FastAPI(title="Audio Transcription API", version="0.1")
 
+
+    
+def generate_JSON():
+    word_timings = print_word_timings()
+    return JSONResponse(content={word_timings}, status_code=200)
+
+    
+    
+    
+    
 @app.get("/")
-def read_root():
-    return {"status": "Backend Test OK"}
+def word_timings():
+    return print_word_timings()
