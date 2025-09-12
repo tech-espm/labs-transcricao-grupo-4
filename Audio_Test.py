1b22b8c3ef6352ebfc58701304f08c73775fc9f9
rafaarklu <169057187+rafaarklu@users.noreply.github.com>
2025-09-12 10:10:20 -0300

    Scrum-25: Criar endpoint POST /api/audio (receber arquivo)

--- Audio_Test.py
+++ Audio_Test.py
@@ -1,19 +1,8 @@
 from openai import OpenAI
 from config import api_key
+import io
 
 client = OpenAI(api_key=api_key)
-audio_file = open(".\Audio\q_isso_renan.mp3", "rb")
-
-transcription = client.audio.transcriptions.create(
-    file=audio_file,
-    model="whisper-1", 
-    response_format="verbose_json",
-    timestamp_granularities=["word"]
-)
-
-
-#for word in transcription.words:
-#   print(f"{(word.start * 1000):.0f}\t{(word.end * 1000):.0f}\t{word.word}")
 
 
 def print_word_timings():
@@ -25,12 +14,41 @@ def print_word_timings():
             response_format="verbose_json",
             timestamp_granularities=["word"]
         )
+    
     return [
         {
-            "start": word,
-            "end": word,
-            "word": word
+            "start": word.start,
+            "end": word.end,
+            "word": word.word
         }
         for word in transcription.words
     ]
+
     
+
+
+
+def get_word_timings_from_file_bytes(file_bytes, filename):
+    # Cria um buffer em memória a partir do arquivo enviado
+    audio_buffer = io.BytesIO(file_bytes)
+    audio_buffer.name = filename  # importante: define o nome com extensão correta
+    
+    transcription = client.audio.transcriptions.create(
+        file=audio_buffer,
+        model="whisper-1",
+        response_format="verbose_json",
+        timestamp_granularities=["word"]
+    )
+    
+    return [
+        {
+            "start": word.start,
+            "end": word.end,
+            "word": word.word
+        }
+        for word in transcription.words
+    ]
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
-from Audio_Test import print_word_timings
+from Audio_Test import print_word_timings, get_word_timings_from_file_bytes
 from fastapi.responses import JSONResponse
+from fastapi import FastAPI, UploadFile, File
+from openai import OpenAI
+from config import api_key
 
 
 # LEMBRETE!!!!!!!! == SEMPRE QUE ABRIR A API NO NAEGADOR ADICIONAR /docs NO FINAL DO LINK 
 # PARA VER A DOCUMENTAÇÃO AUTOMÁTICA
 # EXEMPLO: http://http://127.0.0.1:8000/docs
-app = FastAPI(title="Audio Transcription API", version="0.1")
+app = FastAPI(title="Audio Transcription API", version="0.2")
+  
 
-
-    
-def generate_JSON():
-    word_timings = print_word_timings()
-    return JSONResponse(content={word_timings}, status_code=200)
-
-    
-    
-    
     
 @app.get("/")
 def word_timings():
     return print_word_timings()
+
+
+
+@app.post("/upload-audio/")
+async def upload_audio(file: UploadFile = File(...)):
+    try:
+        file_bytes = await file.read()
+        timings = get_word_timings_from_file_bytes(file_bytes, file.filename)
+        return JSONResponse(content=timings)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
