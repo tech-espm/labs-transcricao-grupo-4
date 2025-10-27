1fc47f3f8f9183b100a2374d382dd16c56ddb124
rafaarklu <169057187+rafaarklu@users.noreply.github.com>
2025-10-27 15:53:42 -0300

    FUNCIONOU!!, transcrição aparecendo no html

--- main.py
+++ main.py
@@ -18,12 +18,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
     
